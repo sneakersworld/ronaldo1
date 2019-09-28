7b56e3a952539ea1204699c0668c8240a253c85f
Mike <Mike@Walters-MacBook-Air.local>
2019-09-28 04:13:31 -0700

    Fixed filter buttons and shoe flatlist scroll problem

--- src/containers/ExplorePage.js
+++ src/containers/ExplorePage.js
@@ -7,20 +7,65 @@
 
 'use strict';
 
-import React, { Component } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, TpouchableOpacity, View } from 'react-native';
-import { SearchBar, Icon } from 'react-native-elements';
+import React, { Component, Fragment } from 'react';
+import { FlatList, SafeAreaView, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { SearchBar, Icon, Card } from 'react-native-elements';
 
 import Colors from '../Colors';
 import ShoeItem from './ShoeItem';
 import sampleData from '../data/shoes';
 
+
 class ExplorePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: ''
+      search: '',
     };
+  }
+
+  filters = ["Most Popular", "Most Recent", "Cheapest", "Men", "Women"];
+  
+
+
+  _keyExtractor = _ => _.sku.toString();
+
+  onFilterButtonPress = (item) => {
+
+  }
+
+  updateSearch = (search) => this.setState({search});
+
+
+  renderHeader = () => {
+    return (
+      <Fragment>
+        <SearchBar 
+          placeholder="Search" 
+          onChangeText={this.updateSearch} 
+          value={this.state.search} 
+          inputContainerStyle={styles.searchBarInner} 
+          inputStyle={styles.searchBarInner} 
+          containerStyle={styles.searchBarOuter}
+        />
+        <FlatList 
+          style={styles.filterList}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={this.filters} 
+          renderItem={this.renderFilter}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </Fragment>
+    );
+  }
+
+  renderFilter = (item) => {
+    return (
+        <TouchableHighlight underlayColor='blue' onPress={this.onFilterButtonPress} style={styles.filterButton}>
+          <Text>{item.item}</Text>
+        </TouchableHighlight>
+      );
   }
 
   _renderItem = ({item}) => (
@@ -33,46 +78,19 @@ class ExplorePage extends Component {
     />
   )
 
-  _keyExtractor = _ => _.sku.toString();
-  filters = ["Most Popular", "Most Recent", "Cheapest", "Men", "Women"];
-
-  renderFilter = (item) => {
-    return (
-        <View style={styles.filterItem}>
-          <Text>{item.item}</Text>
-        </View>
-      );
-  }
-
-  updateSearch = (search) => this.setState({search});
-
   render() {
     return (
       <SafeAreaView style={styles.screenContainer}>
         <View style={styles.container}>
-          <SearchBar 
-            placeholder="Search" 
-            onChangeText={this.updateSearch} 
-            value={this.state.search} 
-            inputContainerStyle={styles.searchBarInner} 
-            inputStyle={styles.searchBarInner} 
-            containerStyle={styles.searchBarOuter}
-          />
-
-          <FlatList 
-            horizional
-            data={this.filters} 
-            renderItem={this.renderFilter}
-            keyExtractor={(item, index) => index.toString()}
-            style={styles.filterlist}
-          />
-
           <FlatList 
             style={styles.list}
             data={sampleData}
+            ListHeaderComponent={this.renderHeader}
+            ListHeaderComponentStyle={styles.header}
             renderItem={this._renderItem} 
             numColumns={2}
             keyExtractor={this._keyExtractor}
+            showsVerticalScrollIndicator={false}
           />
         </View>
       </SafeAreaView>
@@ -87,15 +105,14 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.lighter
   },
 
-  title: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  header: {
+    backgroundColor: Colors.lighter,
+    marginBottom:10
   },
 
   searchBarInner: {
     backgroundColor: Colors.white,
-    marginHorizontal: 3,
+    marginHorizontal: 0,
     borderTopWidth: 0,
     borderBottomWidth: 0
   },
@@ -106,15 +123,20 @@ const styles = StyleSheet.create({
     borderBottomWidth: 0
   },
 
-  filterlist: {
-    marginHorizontal: 10
+  filterList: {
+    marginLeft: 7,
+    marginRight: 7
   },
 
-  filterItem: {
-    padding: 3,
-    backgroundColor: 'white',
-    width: 100,
-    alignItems: 'center'
+  filterButton: {
+    backgroundColor: Colors.white,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'blue',
+    alignItems: 'center',
+    width:100,
+    marginHorizontal:1,
+    padding:5
   }
 });
 
